182663eff64ebc108bcf819475d6fecd39873585
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2022-12-10 23:55:30 -0800

    test

--- src/config/config-helper.ts
+++ src/config/config-helper.ts
@@ -1,16 +1,21 @@
 import * as dotenv from "dotenv";
-
-const REQUIRED_PROPS = ['USER_NAME', 'PASSWORD'];
+import * as fs from "fs";
+import * as path from "path";
 
 export function loadConfig() {
     dotenv.config();
-    for (let prop of REQUIRED_PROPS) {
+    const exampleProps = getEnvExampleProps();
+    for (let prop of exampleProps) {
         if (!process.env[prop]) {
             throw new Error(`${prop} env property cannot be empty`);
         }
     }
 }
 
+function getEnvExampleProps() {
+    const content = fs.readFileSync(path.resolve(__dirname, '../../.env.example'), {encoding: 'utf8'});
+    return content.split('\n').filter((prop) => !prop.startsWith("#")).map((prop) => prop.split('=')[0]);
+}
 
 
-export const USER_NAME = process.env.USER_NAME as string
+export const USER_NAME = process.env.USER_NAME as string;
